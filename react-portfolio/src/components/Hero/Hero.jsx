bca986df9a6b2fdc058882c70a5845bef4326a85
Shah Dhruv Piyushkumar <shahdhruvp16@gmail.com>
2025-03-15 16:10:12 +0530

    Website fully devloped

---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Dhruv</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 2 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:shahdhruvp16@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("D:/InnomaticsInternshipProject3/react-portfolio/assets/hero/heroimg.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
+export default Hero
